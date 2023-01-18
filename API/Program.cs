a107dedcfdb2356c7cdff35bb3a7ee5339c1ff2b
Debopriya Roy <dpriya.jsr@gmail.com>
2023-01-19 02:43:52 +0530

    Login Successful

--- API/Program.cs
+++ API/Program.cs
@@ -5,10 +5,12 @@ using Microsoft.Azure.Cosmos;
 var builder = WebApplication.CreateBuilder(args);
 var primaryKey = builder.Configuration["CosmosDbPrimaryKey"];
 var endpoint = builder.Configuration["CosmosDbEndpoint"];
+var token = builder.Configuration["Token"];
 
 builder.Services.AddScoped<PatientOperations>();
 builder.Services.AddScoped<PatientRepository>();
 builder.Services.AddScoped(sp => new CosmosClient(endpoint, primaryKey));
+builder.Services.AddScoped(sp => token);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
--- DAL/Repositories/PatientRepository.cs
+++ DAL/Repositories/PatientRepository.cs
@@ -1,4 +1,5 @@
 ﻿using Azure;
+using Common.DTO;
 using Common.Models;
 using DAL.Models;
 using Microsoft.Azure.Cosmos;
@@ -19,9 +20,9 @@ namespace DAL.Repositories
 
         public async Task<Boolean> EmailExistsAsync(string email)
         {
-            var query = $"SELECT Patient.email FROM Patient WHERE Patient.email = @email";
+            var query = $"SELECT * FROM Patient WHERE Patient.email = @email";
             var queryDefinition = new QueryDefinition(query).WithParameter("@email", email);
-            var emailResponse = this.container.GetItemQueryIterator<string>(queryDefinition);
+            var emailResponse = this.container.GetItemQueryIterator<PatientData>(queryDefinition);
             var response = await emailResponse.ReadNextAsync();
             if (response.Resource.FirstOrDefault() == null)
             {
@@ -33,18 +34,24 @@ namespace DAL.Repositories
 
         private string CreatePasswordHash(string password)
         {
-            using(var hmac = new HMACSHA512())
+            using (var hmac = new HMACSHA512(System.Text.Encoding.UTF8.GetBytes("salt")))
             {
                 var hashedPasswordByte = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
-                password = System.Text.Encoding.UTF8.GetString(hashedPasswordByte);
+                password = Convert.ToBase64String(hashedPasswordByte);
             }
 
             return password;
         }
 
+        private bool VerifyPasswordHash(string passwordInput, string passwordOriginal)
+        {
+            passwordInput = CreatePasswordHash(passwordInput);
+            return String.Equals(passwordInput, passwordOriginal);
+        }
+
         public async Task<PatientData> CreatePatientAsync(string email, string password, string name, string phoneNumber, string dateOfBirth)
         {
-            var individual = new PatientData()
+            var patient = new PatientData()
             {
                 Id = Guid.NewGuid().ToString(),
                 Email = email,
@@ -54,8 +61,35 @@ namespace DAL.Repositories
                 DateOfBirth = dateOfBirth
             };
 
-            var individualCreated = await container.CreateItemAsync<PatientData>(individual);
-            return individualCreated.Resource;
+            var patientCreated = await container.CreateItemAsync<PatientData>(patient);
+            return patientCreated.Resource;
+        }
+
+        public async Task<IPatient> LoginPatientAsync(string email, string password)
+        {
+            var query = $"SELECT * FROM Patient WHERE Patient.email = @email";
+            var queryDefinition = new QueryDefinition(query).WithParameter("@email", email);
+            var patientResponse = this.container.GetItemQueryIterator<PatientData>(queryDefinition);
+            var response = await patientResponse.ReadNextAsync();
+            if (response.Resource.FirstOrDefault() == null)
+            {
+                return null;
+            }
+
+            if (!VerifyPasswordHash(password, response.Resource.FirstOrDefault().Password))
+            {
+                return null;
+            }
+
+            var patient = new Patient()
+            {
+                Name = response.Resource.FirstOrDefault().Name,
+                Email = response.Resource.FirstOrDefault().Email,
+                PhoneNumber = response.Resource.FirstOrDefault().PhoneNumber,
+                DateOfBirth = response.Resource.FirstOrDefault().DateOfBirth,
+            };
+
+            return patient;
         }
     }
 }
