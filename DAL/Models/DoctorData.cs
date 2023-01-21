4c03145d10b3428a76e86691ddf67ae47d1ab92b
Debopriya Roy <dpriya.jsr@gmail.com>
2023-01-22 02:21:41 +0530

    Doctor model created

--- DAL/Models/DoctorData.cs
+++ DAL/Models/DoctorData.cs
@@ -1,6 +1,42 @@
-﻿namespace DAL.Models
+﻿using Common.Models;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace DAL.Models
 {
-    internal class DoctorData
+    [JsonObject(NamingStrategyType = typeof(CamelCaseNamingStrategy))]
+    public class DoctorData : IDoctor
     {
+        public DoctorData()
+        {
+        }
+
+        public DoctorData(DoctorData doctor)
+        {
+            Id = doctor.Id;
+            Name = doctor.Name;
+            Email = doctor.Email;
+            Password = doctor.Password;
+            RegistrationNumber = doctor.RegistrationNumber;
+            PhoneNumber = doctor.PhoneNumber;
+        }
+
+        [JsonProperty(PropertyName = "id")]
+        public string Id { get; set; }
+
+        [JsonProperty(PropertyName = "email")]
+        public string Email { get; set; }
+
+        [JsonProperty(PropertyName = "password")]
+        public string Password { get; set; }
+
+        [JsonProperty(PropertyName = "name")]
+        public string Name { get; set; }
+
+        [JsonProperty(PropertyName = "registrationNumber")]
+        public string RegistrationNumber{ get; set; }
+
+        [JsonProperty(PropertyName = "phone")]
+        public string PhoneNumber { get; set; }
     }
 }
