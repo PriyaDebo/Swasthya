527ba10c5d99f87ee4a8a9a265005ac31e2c3bf1
Debopriya Roy <dpriya.jsr@gmail.com>
2023-01-23 00:50:08 +0530

    Hospital model and dto created

---
+++ Common/DTO/Hospital.cs
@@ -0,0 +1,19 @@
+﻿using Common.Models;
+
+namespace Common.DTO
+{
+    public class Hospital : IHospital
+    {
+        public string Id { get; set; }
+
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+
+        public string Name { get; set; }
+
+        public string Address { get; set; }
+
+        public string PhoneNumber { get; set; }
+    }
+}
