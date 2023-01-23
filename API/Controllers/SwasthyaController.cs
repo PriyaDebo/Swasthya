05e73dc7c0ebc5bd9a77f17ff1462b14299c9d96
Debopriya Roy <dpriya.jsr@gmail.com>
2023-01-24 01:05:54 +0530

    Hospital controller added

--- API/Controllers/SwasthyaController.cs
+++ API/Controllers/SwasthyaController.cs
@@ -1,10 +1,12 @@
 using API.Extensions;
 using BL.Operations;
 using Common.ApiRequestModels.DoctorRequestModels;
+using Common.ApiRequestModels.HospitalRequestModels;
 using Common.ApiRequestModels.PatientRequestModels;
 using Common.ApiResponseModels;
 using Microsoft.AspNetCore.Mvc;
 using System.Globalization;
+using System.Runtime.InteropServices;
 
 namespace API.Controllers
 {
@@ -14,11 +16,13 @@ namespace API.Controllers
     {
         readonly PatientOperations patientOperations;
         readonly DoctorOperations doctorOperations;
+        readonly HospitalOperations hospitalOperations;
 
-        public SwasthyaController(PatientOperations patientOperations, DoctorOperations doctorOperations)
+        public SwasthyaController(PatientOperations patientOperations, DoctorOperations doctorOperations, HospitalOperations hospitalOperations)
         {
             this.patientOperations = patientOperations;
             this.doctorOperations = doctorOperations;
+            this.hospitalOperations = hospitalOperations;
         }
 
         [HttpPut]
@@ -70,7 +74,7 @@ namespace API.Controllers
 
         [HttpPost]
         [Route("Login/Patient")]
-        public async Task<ActionResult<PatientResponseModel>> LoginPatientAsync(LoginPatientRequest request)
+        public async Task<ActionResult<string>> LoginPatientAsync(LoginPatientRequest request)
         {
             if (request.Email == null)
             {
@@ -133,7 +137,7 @@ namespace API.Controllers
 
         [HttpPost]
         [Route("Login/Doctor")]
-        public async Task<ActionResult<DoctorResponseModel>> LoginDoctorAsync(LoginDoctorRequest request)
+        public async Task<ActionResult<string>> LoginDoctorAsync(LoginDoctorRequest request)
         {
             if (request.Email == null)
             {
@@ -150,6 +154,69 @@ namespace API.Controllers
             if (response == null)
             {
                 return BadRequest("Login Failed.");
+            }
+
+            return Ok(response);
+        }
+
+        [HttpPut]
+        [Route("Register/Hospital")]
+        public async Task<ActionResult<HospitalResponseModel>> RegisterHospitalAsync(RegisterHospitalRequest request)
+        {
+            if (request.Email == null)
+            {
+                return BadRequest("Email should not be empty.");
+            }
+
+            if (request.Password == null)
+            {
+                return BadRequest("Password should not be empty.");
+            }
+
+            if (request.Name == null)
+            {
+                return BadRequest("Name should not be empty.");
+            }
+
+            if (request.PhoneNumber == null)
+            {
+                return BadRequest("Phone number should not be empty.");
+            }
+
+            if (request.Address == null)
+            {
+                return BadRequest("Address should not be empty.");
+            }
+
+            var hospital = await hospitalOperations.RegisterHospital(request.Email, request.Password, request.Name, request.Address, request.PhoneNumber);
+
+            if (hospital == null)
+            {
+                return BadRequest("Email already registered.");
+            }
+
+            return Ok(hospital.ToAPIModel());
+        }
+
+        [HttpPost]
+        [Route("Login/Hospital")]
+        public async Task<ActionResult<string>> LoginHospitalAsync(LoginHospitalRequest request)
+        {
+            if (request.Email == null)
+            {
+                return BadRequest("Email should not be empty.");
+            }
+
+            if (request.Password == null)
+            {
+                return BadRequest("Password should not be empty.");
+            }
+
+            var response = await hospitalOperations.LoginHospitalAsync(request.Email, request.Password);
+
+            if (response == null)
+            {
+                return BadRequest("Login Failed");
             }
 
             return Ok(response);
---
+++ API/Extensions/HospitalExtension.cs
@@ -0,0 +1,21 @@
+﻿using Common.ApiResponseModels;
+using Common.Models;
+
+namespace API.Extensions
+{
+    public static class HospitalExtension
+    {
+        public static HospitalResponseModel ToAPIModel(this IHospital hospital)
+        {
+            var model = new HospitalResponseModel
+            {
+                Email = hospital.Email,
+                Name = hospital.Name,
+                PhoneNumber = hospital.PhoneNumber,
+                Address = hospital.Address
+            };
+
+            return model;
+        }
+    }
+}
