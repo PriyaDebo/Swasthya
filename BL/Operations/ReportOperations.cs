6f453d0d1aa5b24c03abf19239be747a0839cac3
Debopriya Roy <dpriya.jsr@gmail.com>
2023-01-25 02:08:55 +0530

    Report operations added

---
+++ BL/Operations/ReportOperations.cs
@@ -0,0 +1,33 @@
+﻿using Common.Models;
+using DAL.Repositories;
+
+namespace BL.Operations
+{
+    public class ReportOperations
+    {
+        ReportRepository reportRepository;
+
+        public ReportOperations(ReportRepository reportRepository)
+        {
+            this.reportRepository = reportRepository;
+        }
+
+        public async Task<IReport> AddReportAsync(string email, string title, string report)
+        {
+            var reportResponse = await reportRepository.AddReportAsync(email, title, report);
+            return reportResponse;
+        }
+
+        public async Task<IReport> GetReportByIdAsync(string id)
+        {
+            var reportResponse = await reportRepository.GetReportByIdAsync(id);
+            return reportResponse;
+        }
+
+        public async Task<IEnumerable<IReport>> GetReportsByEmailAsync(string email)
+        {
+            var reportsResponse = await reportRepository.GetReportsByEmailAsync(email);
+            return reportsResponse;
+        }
+    }
+}
