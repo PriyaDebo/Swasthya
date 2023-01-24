fb696b1d1710c676e6905fa37fb5fa7a180bf8d0
Debopriya Roy <dpriya.jsr@gmail.com>
2023-01-24 21:36:46 +0530

    Added report model and dto

---
+++ Common/DTO/Report.cs
@@ -0,0 +1,13 @@
+﻿using Common.Models;
+
+namespace Common.DTO
+{
+    public class Report : IReport
+    {
+        public string Id { get; set; }
+
+        public string Email { get; set; }
+
+        public string MedicalReport { get; set; }
+    }
+}
---
+++ Common/Models/IReport.cs
@@ -0,0 +1,11 @@
+﻿namespace Common.Models
+{
+    public interface IReport
+    {
+        string Id { get; }
+
+        string Email { get; }
+
+        string MedicalReport { get; }
+    }
+}
