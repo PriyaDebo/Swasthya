22f1fe005e5f449d680d33aa5255cc8e3e0244eb
Debopriya Roy <dpriya.jsr@gmail.com>
2023-02-20 23:42:19 +0530

    Patient Registration

--- Common/ApiRequestModels/PatientRequestModels/RegisterPatientRequest.cs
+++ Common/ApiRequestModels/PatientRequestModels/RegisterPatientRequest.cs
@@ -4,16 +4,19 @@ namespace Common.ApiRequestModels.PatientRequestModels
 {
     public class RegisterPatientRequest
     {
+
         [Required]
+        public string Name { get; set; }
+
+        [Required]
+        [RegularExpression(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$", ErrorMessage = "Please enter a valid email address")]
         public string Email { get; set; }
 
         [Required]
         public string Password { get; set; }
 
         [Required]
-        public string Name { get; set; }
-
-        [Required]
+        [RegularExpression(@"^(\+\d{1,3}\s?)?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}$", ErrorMessage = "Please enter a valid 10 digit phone number")]
         public string PhoneNumber { get; set; }
 
         [Required]
